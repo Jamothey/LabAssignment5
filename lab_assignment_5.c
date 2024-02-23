29526538afe5b52c31c0d34eb0da0ac7b0a61830
James Eyler <jameseyler2000@gmail.com>
2024-02-23 17:24:06 -0500

    lab_assignment_5 updated correctly

--- lab_assignment_5.c
+++ lab_assignment_5.c
@@ -9,15 +9,19 @@ typedef struct node {
 // Returns number of nodes in the linkedList.
 int length(node* head)
 {
-   struct node *tmp = head;
-    int len = 0;
-   while (tmp != NULL)
-   {
-      tmp = tmp->next;
-      len++;
-   }
-
-   return (len);
+	int i = 0; 
+	int j = 0;
+	struct node* current = head;
+	while (current != NULL && j == 0) //goes through linked list and counts the number of nodes that are in the linked list, j = 0 accounts for the first NULL character that is the head
+	{
+		if (current == NULL)
+		{
+			j++; //increments the null so that it will only account for 1 NULL char
+		}
+		i++; //increments i by 1
+		current = current -> next; //increments linked list
+	}
+	return i;
 }
 
 // parses the string in the linkedList
@@ -25,6 +29,17 @@ int length(node* head)
 //  then toCString function wil return "abc"
 char* toCString(node* head)
 {
+	struct node* current = head;
+	int len = length(head);
+	char* string = malloc((len + 1) * sizeof(char));
+	for (int i = 0; i < len && current != NULL; i++)
+	{
+		string[i] = current -> letter;
+		current = current -> next;
+	} 
+
+	string[len] = '\0';
+	return string;
 }
 
 // inserts character to the linkedlist
@@ -33,11 +48,37 @@ char* toCString(node* head)
 // head -> |a|->|b|->|c|->|x|
 void insertChar(node** pHead, char c)
 {
+	struct node* current = *pHead;
+    struct node* tmp = malloc(sizeof(struct node)); // Allocate memory for the new node
+    tmp->letter = c;
+    tmp->next = NULL;
+
+    if (current == NULL) {
+        // If the list is empty, make the new node the head
+        *pHead = tmp;
+    } else {
+        // Traverse to the end of the list and add the new node
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = tmp;
+    }
 }
 
 // deletes all nodes in the linkedList.
 void deleteList(node** pHead)
 {
+	struct node* current = *pHead;
+    struct node* next;
+    
+    while (current != NULL)
+    {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+    
+    *pHead = NULL;
 }
 
 int main(void)
